a9c4c772cbdb924e0bfc3b21ec8c0333fc09318a
CarlosPalmaSantos <114425513+CarlosPalmaSantos@users.noreply.github.com>
2025-03-07 16:38:36 +0100

    Agregar atributos de MaintenanceRecord

--- src/main/java/acme/entities/maintenanceRecords/MaintenanceRecord.java
+++ src/main/java/acme/entities/maintenanceRecords/MaintenanceRecord.java
@@ -4,11 +4,17 @@ package acme.entities.maintenanceRecords;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,8 +25,10 @@ public abstract class MaintenanceRecord extends AbstractEntity {
 
 	private static final long		serialVersionUID	= 1L;
 
+	//TODO revisar ValidMoment
 	@Mandatory
 	@Automapped
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date					maintenanceTimestamp;
 
 	@Mandatory
@@ -28,7 +36,20 @@ public abstract class MaintenanceRecord extends AbstractEntity {
 	@Valid
 	private MaintenanceRecordStatus	status;
 
+	//TODO revisar ValidMoment
 	@Mandatory
 	@Automapped
+	@Temporal(TemporalType.DATE)
 	private Date					nextInspectionDate;
+
+	@Mandatory
+	@Automapped
+	@ValidMoney
+	private Money					estimatedCost;
+
+	@Optional
+	@Automapped
+	@ValidString(max = 255)
+	private String					notes;
+
 }
