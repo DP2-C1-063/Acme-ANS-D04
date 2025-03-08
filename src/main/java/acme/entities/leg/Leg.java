06dec76c53f3606af25222cf65c2a5c83b2f59af
luciaponce-us <lucpongar@alum.us.es>
2025-03-08 01:44:42 +0100

    Legs information closes #8

---
+++ src/main/java/acme/entities/leg/Leg.java
@@ -0,0 +1,77 @@
+
+package acme.entities.leg;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.constraints.ValidFlightNumber;
+import acme.constraints.ValidLeg;
+import acme.entities.aircraft.Aircraft;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@ValidLeg
+public class Leg extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidFlightNumber
+	@Column(unique = true)
+	private String				flightNumber;
+
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledArrival;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Status				status;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport				departureAirport;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport				arrivalAirport;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Aircraft			aircraft;
+
+
+	@Transient
+	public int getDuration() {
+
+		long durationInMs = this.scheduledArrival.getTime() - this.scheduledDeparture.getTime();
+		long durationInHours = durationInMs / (1000 * 60 * 60);
+
+		return (int) durationInHours;
+	}
+
+}
