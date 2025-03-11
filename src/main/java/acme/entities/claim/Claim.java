eac8279b8d498886ab1457443d1b70a7656dd432
Mtolgon <mantolgon@alum.us.es>
2025-03-11 17:08:07 +0100

    arreglo claim

--- src/main/java/acme/entities/claim/Claim.java
+++ src/main/java/acme/entities/claim/Claim.java
@@ -14,6 +14,7 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
+import acme.entities.leg.Leg;
 import acme.realms.assistanceAgent.AssistanceAgent;
 import lombok.Getter;
 import lombok.Setter;
@@ -49,9 +50,9 @@ public class Claim extends AbstractEntity {
 	@Mandatory
 	@Automapped
 	private boolean				accepted;
-	//@Mandatory
-	//@VAlid
-	//@Automapped
-	//private leg leg;
+	@Mandatory
+
+	@Automapped
+	private Leg					leg;
 
 }
