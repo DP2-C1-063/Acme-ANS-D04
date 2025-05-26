e3acb46921030014bfb6168861f2b2b3b84f7049
Raquel Ortega Almirón <114416061+raquelortegaalmiron@users.noreply.github.com>
2025-05-26 21:16:13 +0200

    informe testing

--- src/main/java/acme/entities/booking/Booking.java
+++ src/main/java/acme/entities/booking/Booking.java
@@ -4,7 +4,9 @@ package acme.entities.booking;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -29,6 +31,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidBooking
+
+@Table(indexes = {
+	@Index(columnList = "customer_id"), @Index(columnList = "locatorCode")
+})
 public class Booking extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
